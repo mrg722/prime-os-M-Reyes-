fde532403a788e1183812d2fdcfdf896570c16ea
mrg722 <mar.reyesg@duocuc.cl>
2026-09-23 15:46:12 -0300

    chore: bump Prime OS to v9 and migrate storage

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Configuración global de Prime OS.
-const APP_VERSION = "8.2";
-const STORAGE_KEY = "prime_os_martin_v7_3";
+const APP_VERSION = "9.0";
+const STORAGE_KEY = "prime_os_martin_v9";
 const DRAFT_KEY = "prime_os_draft_v8";
 const LEGACY_KEYS = ["prime_os_martin_v7_2", "prime_os_martin_v7_1", "prime_os_martin_v7", "prime_os_martin_v6_3", "prime_os_martin_v6_2", "prime_os_martin_v6_visual", "prime_os_martin_v5", "prime_os_martin_v4", "prime_os_martin_v3", "prime_os_v1", "forgelog_martin_v2", "forgelog_martin_v1"];
 
