dbe22cdf5b8be11b012b25e661cfcaa90214e12f
mrg722 <mar.reyesg@duocuc.cl>
2026-09-23 16:07:46 -0300

    V10: migrate storage safely from V9

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // Configuración global de Prime OS.
-const APP_VERSION = "9.1";
-const STORAGE_KEY = "prime_os_martin_v9";
-const DRAFT_KEY = "prime_os_draft_v8";
+const APP_VERSION = "10.0";
+const STORAGE_KEY = "prime_os_martin_v10";
+const DRAFT_KEY = "prime_os_draft_v10";
 // Incluye la clave de V7.3–V8.2 para migrar los datos existentes a V9 sin perderlos.
-const LEGACY_KEYS = ["prime_os_martin_v7_3", "prime_os_martin_v7_2", "prime_os_martin_v7_1", "prime_os_martin_v7", "prime_os_martin_v6_3", "prime_os_martin_v6_2", "prime_os_martin_v6_visual", "prime_os_martin_v5", "prime_os_martin_v4", "prime_os_martin_v3", "prime_os_v1", "forgelog_martin_v2", "forgelog_martin_v1"];
+const LEGACY_KEYS = ["prime_os_martin_v9", "prime_os_martin_v8_2", "prime_os_martin_v7_3", "prime_os_martin_v7_2", "prime_os_martin_v7_1", "prime_os_martin_v7", "prime_os_martin_v6_3", "prime_os_martin_v6_2", "prime_os_martin_v6_visual", "prime_os_martin_v5", "prime_os_martin_v4", "prime_os_martin_v3", "prime_os_v1", "forgelog_martin_v2", "forgelog_martin_v1"];
 
 const BACKUP_REMINDER_DAYS = 7;
